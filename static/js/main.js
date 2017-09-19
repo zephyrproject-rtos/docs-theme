351c1aa6f740b8b10a8b2e62ad822b9b05a8d2c9
David B. Kinder <david.b.kinder@intel.com>
2017-09-19 11:41:40 -0700

    theme: JS fix for link targets behind heading
    The previous CSS-based fix using bounding box tricks ends up creating a
    problem where nearby links could be hidden by the bounding box.  This
    JS-based solution doesn't suffer from this problem.
    
    Signed-off-by: David B. Kinder <david.b.kinder@intel.com>

--- static/js/main.js
+++ static/js/main.js
@@ -31,7 +31,6 @@ $(window).on("load",function () {
   $('#secondary-menu-button').click(function() {
     $('#sidebar-first').toggleClass('-expanded-submenu');
   });
-
 });
 
 function doSearch(type, query) {
@@ -45,3 +44,32 @@ function doSearch(type, query) {
       break;
   }
 }
+
+/**
+ * Targets are hiding behind the fixed header
+ * Check an href for an anchor. If exists, and in document, scroll to it.
+ * If href argument ommitted, assumes context (this) is HTML Element,
+ * which will be the case when invoked by jQuery after an event
+ */
+function scroll_if_anchor(href) {
+    href = typeof(href) == "string" ? href : $(this).attr("href");
+    // Static value for now (maybe calculate header size dynamically?)
+    var fromTop = 90;
+    if(href.indexOf("#") == 0) { //i.e., target starts with a #
+        // need to escape any "." in the ID (darn doxygen links)
+        var $target = $(href.replace(/\./g, "\\."));
+        if($target.length) { // Watch for empty references (e.g., just a #)
+            $('html, body').animate({ scrollTop: $target.offset().top - fromTop });
+            if(history && "pushState" in history) {
+                history.pushState({}, document.title, window.location.pathname + href);
+                return false;
+            }
+        }
+    }
+}
+
+// When our page loads, check to see if it contains an anchor
+scroll_if_anchor(window.location.hash);
+
+// Intercept all anchor clicks
+$("body").on("click", "a", scroll_if_anchor);
